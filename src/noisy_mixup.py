232eed8f3c0b3b4879d37f1ef9a21d7a729c0cc7
Francisco Utrera <francisco.utrera@outlook.com>
2021-09-24 09:03:36 -0700

    back to numpy

--- src/noisy_mixup.py
+++ src/noisy_mixup.py
@@ -1,22 +1,21 @@
 import numpy as np
 import torch
 
-BETA = torch.distributions.beta.Beta(torch.tensor([2.], device=torch.device('cuda:0')), 
-                                     torch.tensor([5.], device=torch.device('cuda:0')))
-
 def _noise(x, add_noise_level=0.0, mult_noise_level=0.0, sparsity_level=0.0):
-    add_noise = torch.tensor(0.0, device=torch.device('cuda:0'))
-    mult_noise = torch.tensor(1.0, device=torch.device('cuda:0'))
-    if add_noise_level > 0.0:
-        var = add_noise_level * BETA.sample()
-        add_noise = var * torch.cuda.FloatTensor(x.shape).normal_()
-    if mult_noise_level > 0.0:
-        mult_noise = mult_noise_level * BETA.sample() * (2*torch.cuda.FloatTensor(x.shape).uniform_()-1) + 1 
+    add_noise = 0.0
+    mult_noise = 1.0
+    with torch.cuda.device(0):
+        if add_noise_level > 0.0:
+            var = add_noise_level * np.random.beta(2, 5)
+            add_noise = var * torch.cuda.FloatTensor(x.shape).normal_()
+            #torch.clamp(add_noise, min=-(2*var), max=(2*var), out=add_noise) # clamp
+        if mult_noise_level > 0.0:
+            mult_noise = mult_noise_level * np.random.beta(2, 5) * (2*torch.cuda.FloatTensor(x.shape).uniform_()-1) + 1 
     return mult_noise * x + add_noise      
 
 def do_noisy_mixup(x, y, alpha=0.0, add_noise_level=0.0, mult_noise_level=0.0):
     lam = np.random.beta(alpha, alpha) if alpha > 0.0 else 1.0
-    index = torch.randperm(x.size()[0], device=torch.device('cuda:0'))
+    index = torch.randperm(x.size()[0]).cuda()
     mixed_x = lam * x + (1 - lam) * x[index]
     y_a, y_b = y, y[index]
     return _noise(mixed_x, add_noise_level=add_noise_level, mult_noise_level=mult_noise_level), y_a, y_b, lam
