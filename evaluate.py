6ac4fa1ed94096e098a4dea7b6b62b8b1ce70a9f
Winnie Xu <winnie.xu97@gmail.com>
2021-09-30 12:10:23 -0700

    add code for plotting ece

--- evaluate.py
+++ evaluate.py
@@ -1,31 +1,14 @@
+import argparse
 import os
 
+import numpy as np
 import torch
 import torch.optim
 import torch.utils.data
-import numpy as np
+
 from src.get_data import getData
+from utils import _get_calibration
 
-
-
-def main():
-    
-    folder = 'cifar10_models/'
-    models = [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f))]
-    models = sorted(models)
-    print(models)
-    
-    _, test_loader = getData(name='cifar10', train_bs=128, test_bs=1024) 
- 
-    for index, m in enumerate(models):  
-        model = torch.load(folder + m)
-        
-        #print("Beginning noisy evaluation")
-        print(m)
-        model.eval()
-        #acc1 = cls_validate(test_loader, model)
-        #_ = cls_noisy_validate(test_loader, model)
-        _ = cls_sp_validate(test_loader, model)
 
 def cls_validate(val_loader, model, time_begin=None):
     model.eval()
@@ -33,98 +16,97 @@ def cls_validate(val_loader, model, time_begin=None):
     n = 0
     with torch.no_grad():
         for i, (images, target) in enumerate(val_loader):
-                images = images.cuda(non_blocking=True)
-                target = target.cuda(non_blocking=True)
+            images = images.cuda(non_blocking=True)
+            target = target.cuda(non_blocking=True)
 
-                output = model(images)
-                
-                model_logits = output[0] if (type(output) is tuple) else output
-                pred = model_logits.data.max(1, keepdim=True)[1] # get the index of the max log-probability
-                acc1_val += pred.eq(target.data.view_as(pred)).cpu().sum().item()
-                n += len(images)
+            output = model(images)
+
+            model_logits = output[0] if (type(output) is tuple) else output
+            pred = model_logits.data.max(1, keepdim=True)[1]  # get the index of the max log-probability
+            acc1_val += pred.eq(target.data.view_as(pred)).cpu().sum().item()
+            n += len(images)
 
     avg_acc1 = (acc1_val / n)
     return avg_acc1
 
 
 def cls_noisy_validate(val_loader, model, time_begin=None):
-    
+
     perturbed_test_accs = []
-    for eps in [0, 0.02, 0.04, 0.06, 0.08, 0.1, 0.12, 0.15, 0.18, 0.2, 0.25, 0.3, 0.35]:    
+    for eps in [0, 0.02, 0.04, 0.06, 0.08, 0.1, 0.12, 0.15, 0.18, 0.2, 0.25, 0.3, 0.35]:
         model.eval()
         acc1_val = 0
         n = 0
         with torch.no_grad():
             for i, (images, target) in enumerate(val_loader):
-                    images = images.cuda(non_blocking=True)
-                    target = target.cuda(non_blocking=True)
-    
-    
-                    images += eps * torch.cuda.FloatTensor(images.shape).normal_()
-                    output = model(images)
-        
-                    model_logits = output[0] if (type(output) is tuple) else output
-                    pred = model_logits.data.max(1, keepdim=True)[1] # get the index of the max log-probability
-                    acc1_val += pred.eq(target.data.view_as(pred)).cpu().sum().item()
-                    n += len(images)
-    
+                images = images.cuda(non_blocking=True)
+                target = target.cuda(non_blocking=True)
+
+                images += eps * torch.cuda.FloatTensor(images.shape).normal_()
+                output = model(images)
+
+                model_logits = output[0] if (type(output) is tuple) else output
+                pred = model_logits.data.max(1, keepdim=True)[1]  # get the index of the max log-probability
+                acc1_val += pred.eq(target.data.view_as(pred)).cpu().sum().item()
+                n += len(images)
+
         avg_acc1 = (acc1_val / n)
         perturbed_test_accs.append(avg_acc1)
 
     print(perturbed_test_accs)
     return perturbed_test_accs
-
 
 
 def sp(image, amount):
-      row,col = image.shape
-      s_vs_p = 0.5
-      out = np.copy(image)
-      # Salt mode
-      num_salt = np.ceil(amount * image.size * s_vs_p)
-      idx = np.random.choice(range(32*32), np.int(num_salt), False)
-      out = out.reshape(image.size, -1)
-      out[idx] = np.min(out)
-      out = out.reshape(32,32)
-      
-      # Pepper mode
-      num_pepper = np.ceil(amount * image.size * (1. - s_vs_p))
-      idx = np.random.choice(range(32*32), np.int(num_pepper), False)
-      out = out.reshape(image.size, -1)
-      out[idx] = np.max(out)
-      out = out.reshape(32,32)
-      return out
-  
+    row, col = image.shape
+    s_vs_p = 0.5
+    out = np.copy(image)
+    # Salt mode
+    num_salt = np.ceil(amount * image.size * s_vs_p)
+    idx = np.random.choice(range(32 * 32), np.int(num_salt), False)
+    out = out.reshape(image.size, -1)
+    out[idx] = np.min(out)
+    out = out.reshape(32, 32)
+
+    # Pepper mode
+    num_pepper = np.ceil(amount * image.size * (1. - s_vs_p))
+    idx = np.random.choice(range(32 * 32), np.int(num_pepper), False)
+    out = out.reshape(image.size, -1)
+    out[idx] = np.max(out)
+    out = out.reshape(32, 32)
+    return out
+
+
 def sp_wrapper(data, amount):
     np.random.seed(12345)
     for i in range(data.shape[0]):
-        data_numpy = data[i,0,:,:].data.cpu().numpy()
+        data_numpy = data[i, 0, :, :].data.cpu().numpy()
         noisy_input = sp(data_numpy, amount)
-        data[i,0,:,:] = torch.tensor(noisy_input).float().to('cuda')
+        data[i, 0, :, :] = torch.tensor(noisy_input).float().to('cuda')
 
-    return data    
+    return data
+
 
 def cls_sp_validate(val_loader, model, time_begin=None):
-    
+
     perturbed_test_accs = []
-    for eps in [0, 0.02, 0.04, 0.06, 0.08, 0.1, 0.12, 0.15, 0.18, 0.2, 0.25, 0.3, 0.35]:    
+    for eps in [0, 0.02, 0.04, 0.06, 0.08, 0.1, 0.12, 0.15, 0.18, 0.2, 0.25, 0.3, 0.35]:
         model.eval()
         acc1_val = 0
         n = 0
         with torch.no_grad():
             for i, (images, target) in enumerate(val_loader):
-                    images = images.cuda(non_blocking=True)
-                    target = target.cuda(non_blocking=True)
-    
-    
-                    images = sp_wrapper(images, eps)
-                    output = model(images)
-        
-                    model_logits = output[0] if (type(output) is tuple) else output
-                    pred = model_logits.data.max(1, keepdim=True)[1] # get the index of the max log-probability
-                    acc1_val += pred.eq(target.data.view_as(pred)).cpu().sum().item()
-                    n += len(images)
-    
+                images = images.cuda(non_blocking=True)
+                target = target.cuda(non_blocking=True)
+
+                images = sp_wrapper(images, eps)
+                output = model(images)
+
+                model_logits = output[0] if (type(output) is tuple) else output
+                pred = model_logits.data.max(1, keepdim=True)[1]  # get the index of the max log-probability
+                acc1_val += pred.eq(target.data.view_as(pred)).cpu().sum().item()
+                n += len(images)
+
         avg_acc1 = (acc1_val / n)
         perturbed_test_accs.append(avg_acc1)
 
@@ -132,5 +114,73 @@ def cls_sp_validate(val_loader, model, time_begin=None):
     return perturbed_test_accs
 
 
+def main(folder, eval_fn=cls_sp_validate):
+
+    models = [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f))]
+    models = sorted(models)
+    print(models)
+
+    _, test_loader = getData(name='cifar10', train_bs=128, test_bs=1024)
+
+    info_dict = {}
+
+    for index, m in enumerate(models):
+        model = torch.load(folder + m)
+
+        #print("Beginning noisy evaluation")
+        print(m)
+        model.eval()
+        #acc1 = cls_validate(test_loader, model)
+        #_ = cls_noisy_validate(test_loader, model)
+        result_m = eval_fn(test_loader, model)
+        info_dict[m] = result_m
+
+    return info_dict
+
+
+def get_calibration(data_loader, model, debug=False):
+    model.eval()
+    calibration = None
+    preds_mean = None
+    with torch.no_grad():
+        for i, (images, target) in enumerate(data_loader):
+            images = images.cuda(non_blocking=True)
+            target = target.cuda(non_blocking=True)  # TODO: one hot or class number? We want class number
+
+            output = model(images)
+
+            model_logits = output[0] if (type(output) is tuple) else output
+            preds = model_logits.data.mean(0, keepdim=True)  # .max(1, keepdim=True)[1]  # get the index of the max log-probability
+            if preds_mean = None:
+                preds_mean = preds
+                continue
+            preds_mean = torch.cat((preds_mean, preds)) # TODO: are these unnormalized log probabilitiies? We want normalized probabilities
+
+    reliability = _get_calibration(target, preds_mean, debug=False)
+    return reliability
+
+
+def main_plot(folder, save_dir):
+    models = [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f))]
+    models = sorted(models)
+    for index, m in enumerate(models):
+        print(f'model: {m} ({index + 1}/{len(models)})')
+        plot_reliability(save_dir, os.path.basename(m))
+
+
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser("Noisy Feature Mixup")
+    parser.add_argument("--dir", type=str, default='cifar10_models/', required=True, help='model dir')
+    parser.add_argument("--save_dir", default='./', help='if plotting, save here')
+    parser.add_argument("--plot", action="store_true", default=False, help="whether or not to plot")
+
+    args = parser.parse_args()
+
+    results_dict = main(args.dir, get_calibration)
+    with open(os.path.join(args.save_dir, 'reliability.npy'), 'wb') as f:
+        np.save(f, results_dict)
+        print(f'saved results successfully')
+
+    if args.plot:
+        main_plot(args.dir, args.save_dir)
+        print(f'plots saved in {args.save_dir}')
---
+++ utils.py
@@ -0,0 +1,125 @@
+import pickle
+
+import numpy as np
+import torch
+
+
+def _get_calibration(y, p_mean, num_bins=20, axis=-1, individual=False, attributes=False, debug=False):
+    """Compute the calibration.
+    Modified from: https://github.com/xwinxu/bayesian-sde/blob/main/brax/utils/utils.py
+    References:
+    https://arxiv.org/abs/1706.04599
+    https://arxiv.org/abs/1807.00263
+    Args:
+      y: class labels (binarized)
+      p_mean: numpy array, size (batch_size, num_classes)
+            containing the mean output predicted probabilities
+      num_bins: number of bins
+      axis: Axis of the labels
+      individual: Return ECE for individual labels
+      attributes: Return micro average of ECE scores across attributes
+      debug: Return debug information
+    Returns:
+      cal: a dictionary
+      {
+        reliability_diag: realibility diagram
+        ece: Expected Calibration Error
+        nb_items: nb_items_bin/np.sum(nb_items_bin)
+    }
+    """
+    assert not (individual and micro)
+
+    if attributes:
+        y = y.reshape(-1)
+        p_mean = p_mean.reshape(-1, p_mean.shape[2])
+
+    # compute predicted class and its associated confidence (probability)
+    conf, class_pred = p_mean.max(axis)
+
+    assert y.shape[0] == p_mean.shape[0]
+    assert len(p_mean.shape) == len(y.shape) + 1
+    assert p_mean.shape[1] > 1  # features
+    assert len(y.shape) == 1
+
+    tau_tab = torch.linspace(0, 1, num_bins + 1, device=p_mean.device)
+
+    conf = conf[None]
+    for _ in range(len(y.shape)):
+        tau_tab = tau_tab.unsqueeze(-1)
+
+    sec = (conf < tau_tab[1:]) & (conf >= tau_tab[:-1])
+
+    nb_items_bin = sec.sum(1)
+
+    mean_conf = (conf * sec).sum(1) / nb_items_bin
+    acc_tab = ((class_pred == y)[None] * sec).sum(1) / nb_items_bin
+
+    _weights = nb_items_bin.float() / nb_items_bin.sum(0)
+    ece = ((mean_conf - acc_tab).abs() * _weights).nansum(0)
+    if not individual:
+        # pytorch doesn't have a built in nanmean
+        ece[ece.isnan()] = 0
+        ece = ece.mean(0)
+
+    cal = {
+        'reliability_diag': (mean_conf, acc_tab),
+        'ece': ece,
+        '_weights': _weights,
+    }
+    if debug:
+        cal.update({'conf': conf, 'sec': sec, 'tau_tab': tau_tab, 'acc_tab': acc_tab, 'p_mean': p_mean})
+    return cal
+
+
+def _plot_reliability(save_dir, m, reliability_diag, ece, freq, dset_label_info):
+    plt.clf()
+
+    with open(f"{save_dir}/cache_ece_{m}.pickle", "wb") as f:
+        d = {"reliability_diag": reliability_diag, "ece": ece, "freq": freq, "dset_label_info": dset_label_info}
+        pickle.dump(d, f, protocol=4)
+
+    conf, acc = reliability_diag
+    conf[conf.isnan()] = 0
+    acc[acc.isnan()] = 0
+
+    num_bins = 20
+    tau_tab = torch.linspace(0, 1, num_bins + 1)
+    binned_confs = tau_tab[torch.searchsorted(tau_tab, conf)]
+
+    font = {'family': 'normal', 'size': 20}
+    plt.rc('font', **font)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.set_aspect('equal', adjustable='box')
+
+    plt.bar(binned_confs, acc, color="blue", linewidth=.1, edgecolor="black", align="edge", width=1 / num_bins)
+    plt.plot(np.linspace(0, 1, 1000), np.linspace(0, 1, 1000), "--")
+    plt.annotate(f"ECE: {ece * 100:.2f}%",
+                 xy=(140, 240),
+                 xycoords='axes points',
+                 size=20,
+                 ha='right',
+                 va='top',
+                 bbox=dict(boxstyle='round', fc="#f6b2b2", color="#f6b2b2"))
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.0])
+    plt.xticks([.0, .5, 1.])
+    plt.yticks([.0, .5, 1.])
+    plt.xlabel('Confidence')
+    plt.ylabel('Accuracy')
+    plt.title('')
+    plt.gcf().subplots_adjust(right=.95, left=.14, bottom=.15)
+    plt.savefig(f"{save_dir}/ece_{m}.png", dpi=200)
+    plt.savefig(f"{save_dir}/ece_{m}.pdf")
+    plt.close(fig)
+
+
+def plot_reliability(save_dir, m, reliability_diag=None, ece=None, freq=None, dset_label_info=None, from_cache=True):
+    if from_cache:
+        with open(f"{save_dir}/reliability.npy", "rb") as f:
+            d = np.load(f, allow_pickle=True).item()
+    else:
+        d = {"reliability_diag": reliability_diag, "ece": ece, "freq": freq, "dset_label_info": dset_label_info}
+
+    _plot_reliability(save_dir, m, **d)
