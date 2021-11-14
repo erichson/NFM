0e0d5311f6a10e207acdc16bf40187c780e56cc0
Winnie Xu <winnie.xu97@gmail.com>
2021-11-14 00:51:12 +0000

    cifar10c plot and experiment

--- cifar10c_evaluate.py
+++ cifar10c_evaluate.py
@@ -6,33 +6,33 @@ import torch
 import torch.optim
 import torch.utils.data
 
-from get_data import NOISE_TYPES, SEVERITIES
+from src.get_data import NOISE_TYPES, SEVERITIES
 from src.get_data import getData
-from utils import _get_calibration
+from utils import _get_calibration, plot_reliability, plot_cifar10c
 
+import collections
 
-def main(folder, eval_fn=cls_sp_validate):
+def cls_validate(val_loader, model, time_begin=None):
+    model.eval()
+    acc1_val = 0
+    n = 0
+    with torch.no_grad():
+        for i, (images, target) in enumerate(val_loader):
+            images = images.cuda(non_blocking=True)
+            target = target.cuda(non_blocking=True)
 
-    models = [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f))]
-    models = sorted(models)
-    print(models)
+            output = model(images)
 
-    _, test_loader = getData(name='cifar10', train_bs=128, test_bs=1024)
+            model_logits = output[0] if (type(output) is tuple) else output
+            pred = model_logits.data.max(1, keepdim=True)[1]  # get the index of the max log-probability
+            acc1_val += pred.eq(target.data.view_as(pred)).cpu().sum().item()
+            n += len(images)
 
-    info_dict = {}
+    avg_acc1 = (acc1_val / n)
+    return avg_acc1
 
-    for index, m in enumerate(models):
-        model = torch.load(folder + m)
-
-        print(m)
-        model.eval()
-        result_m = eval_fn(test_loader, model)
-        info_dict[m] = result_m
-
-    return info_dict
-
-
-def main_cifar10c(folder, eval_fn=cls_sp_validate):
+def main_cifar10c(folder, save_dir):
+    os.makedirs(os.path.join(args.save_dir, 'cifar10c'), exist_ok=True)
 
     models = [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f))]
     models = sorted(models)
@@ -48,16 +48,41 @@ def main_cifar10c(folder, eval_fn=cls_sp_validate):
             results[m][noise] = collections.defaultdict(dict)
             for severity in SEVERITIES:
                 _, test_loader = getData(name='cifar10c', train_bs=128, test_bs=1024, severity=severity, noise=noise)
-                result_m = eval_fn(test_loader, model)
+                result_m = cls_validate(test_loader, model)
                 results[m][noise][severity] = result_m
+        with open(f"{save_dir}/cifar10c/robust_{m}.pickle", "wb") as f:
+            np.save(f, result_m)
 
     return results
 
+def main_calibration(folder, save_dir, n_bins=11):
 
-def get_calibration(data_loader, model, debug=False):
+    models = [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f))]
+    models = sorted(models)
+    print(models)
+
+    _, test_loader = getData(name='cifar10', train_bs=1, test_bs=test_batch_size)
+
+    info_dict = {}
+
+    os.makedirs(os.path.join(args.save_dir, 'ece'), exist_ok=True)
+
+    for index, m in enumerate(models):
+        model = torch.load(folder + m)
+
+        print(m)
+        model.eval()
+        result_m = get_calibration(test_loader, model, n_bins)
+        info_dict[m] = result_m
+        with open(f"{save_dir}/ece/cache_ece_{m}.pickle", "wb") as f:
+            np.save(f, result_m)
+
+    return info_dict
+
+def get_calibration(data_loader, model, debug=False, n_bins=11):
     model.eval()
-    calibration = None
-    preds_mean = None
+    mean_conf, mean_acc = [], []
+    ece = []
     with torch.no_grad():
         for i, (images, target) in enumerate(data_loader):
             images = images.cuda(non_blocking=True)
@@ -66,17 +91,20 @@ def get_calibration(data_loader, model, debug=False):
             output = model(images)
 
             model_logits = output[0] if (type(output) is tuple) else output
-            preds = model_logits.data.mean(0, keepdim=True)  # .max(1, keepdim=True)[1]  # get the index of the max log-probability
-            if preds_mean = None:
-                preds_mean = preds
-                continue
-            preds_mean = torch.cat((preds_mean, preds)) # TODO: are these unnormalized log probabilitiies? We want normalized probabilities
+            preds = torch.nn.functional.softmax(model_logits.data)  # .max(1, keepdim=True)[1]  # get the index of the max log-probability
+            print(preds.shape)
+            # preds = model_logits.data.mean(0, keepdim=True)  # .max(1, keepdim=True)[1]  # get the index of the max log-probability
+            calibration_dict = _get_calibration(target, preds, debug=False, num_bins=n_bins)
+            mean_conf.append(calibration_dict['reliability_diag'][0])
+            mean_acc.append(calibration_dict['reliability_diag'][1])
 
-    reliability = _get_calibration(target, preds_mean, debug=False)
-    return reliability
+            ece.append(calibration_dict['ece'])
+
+    calibration_results = {'reliability_diag': (torch.vstack(mean_conf), torch.vstack(mean_acc)), 'ece': torch.vstack(ece)}
+    return calibration_results
 
 
-def main_plot(folder, save_dir):
+def main_plot_reliability(folder, save_dir):
     models = [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f))]
     models = sorted(models)
     for index, m in enumerate(models):
@@ -84,19 +112,35 @@ def main_plot(folder, save_dir):
         plot_reliability(save_dir, os.path.basename(m))
 
 
+def main_plot_cifar10c(folder, save_dir):
+    models = [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f))]
+    models = sorted(models)
+    plot_cifar10c(save_dir, models)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("Noisy Feature Mixup")
-    parser.add_argument("--dir", type=str, default='cifar10_models/', required=True, help='model dir')
-    parser.add_argument("--save_dir", default='./', help='if plotting, save here')
-    parser.add_argument("--plot", action="store_true", default=False, help="whether or not to plot")
+    parser.add_argument("--dir", type=str, default='cifar10_models/', required=False, help='model dir')
+    parser.add_argument("--save_dir", default='.', help='if plotting, save here. Call this the exp id.')
+    parser.add_argument("--plot_calib", action="store_true", default=False, help="whether or not to plot calibration, else just evaluates on cifar10c")
+    parser.add_argument("--batch_size", default=128, type=int, help='batch size')
 
     args = parser.parse_args()
 
-    results_dict = main(args.dir, get_calibration)
-    with open(os.path.join(args.save_dir, 'reliability.npy'), 'wb') as f:
-        np.save(f, results_dict)
-        print(f'saved results successfully')
+    test_batch_size = args.batch_size
+    os.makedirs(args.save_dir, exist_ok=True)
 
-    if args.plot:
-        main_plot(args.dir, args.save_dir)
+    if args.plot_calib:
+        # main_plot_reliability(args.dir, args.save_dir)
+        main_plot_cifar10c(args.dir, args.save_dir)
         print(f'plots saved in {args.save_dir}')
+    else:
+        results_dict1 = main_calibration(args.dir, args.save_dir)
+        with open(os.path.join(args.save_dir, 'reliability.npy'), 'wb') as f:
+            np.save(f, results_dict1)
+            print(f'saved results successfully')
+        # results_dict2 = main_cifar10c(args.dir, args.save_dir)
+        # with open(os.path.join(args.save_dir, 'cifar10c.npy'), 'wb') as f:
+        #     np.save(f, results_dict2)
+        #     print(f'saved results successfully')
+
--- utils.py
+++ utils.py
@@ -1,7 +1,11 @@
+from os import set_blocking
 import pickle
 
 import numpy as np
 import torch
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
 
 
 def _get_calibration(y, p_mean, num_bins=20, axis=-1, individual=False, attributes=False, debug=False):
@@ -51,11 +55,12 @@ def _get_calibration(y, p_mean, num_bins=20, axis=-1, individual=False, attribut
 
     nb_items_bin = sec.sum(1)
 
-    mean_conf = (conf * sec).sum(1) / nb_items_bin
-    acc_tab = ((class_pred == y)[None] * sec).sum(1) / nb_items_bin
+    mean_conf = (conf * sec).sum(1) / (nb_items_bin+1e-8)
+    acc_tab = ((class_pred == y)[None] * sec).sum(1) / (nb_items_bin+1e-8)
 
     _weights = nb_items_bin.float() / nb_items_bin.sum(0)
     ece = ((mean_conf - acc_tab).abs() * _weights).nansum(0)
+
     if not individual:
         # pytorch doesn't have a built in nanmean
         ece[ece.isnan()] = 0
@@ -71,19 +76,23 @@ def _get_calibration(y, p_mean, num_bins=20, axis=-1, individual=False, attribut
     return cal
 
 
-def _plot_reliability(save_dir, m, reliability_diag, ece, freq, dset_label_info):
+def _plot_reliability(save_dir, m, reliability_diag, ece, n_bins=11): #, freq, dset_label_info):
     plt.clf()
 
-    with open(f"{save_dir}/cache_ece_{m}.pickle", "wb") as f:
-        d = {"reliability_diag": reliability_diag, "ece": ece, "freq": freq, "dset_label_info": dset_label_info}
-        pickle.dump(d, f, protocol=4)
+    # with open(f"{save_dir}/cache_ece_{m}.pickle", "wb") as f:
+    #     d = {"reliability_diag": reliability_diag, "ece": ece} # , "freq": freq, "dset_label_info": dset_label_info}
+    #     pickle.dump(d, f, protocol=4)
 
     conf, acc = reliability_diag
     conf[conf.isnan()] = 0
     acc[acc.isnan()] = 0
 
-    num_bins = 20
+    num_bins = n_bins
     tau_tab = torch.linspace(0, 1, num_bins + 1)
+    fix_conf = torch.linspace(0, 1, num_bins)
+
+    # tau_tab = tau_tab.to(conf.device)
+    conf = conf.cpu()
     binned_confs = tau_tab[torch.searchsorted(tau_tab, conf)]
 
     font = {'family': 'normal', 'size': 20}
@@ -93,9 +102,13 @@ def _plot_reliability(save_dir, m, reliability_diag, ece, freq, dset_label_info)
     ax = fig.add_subplot(111)
     ax.set_aspect('equal', adjustable='box')
 
-    plt.bar(binned_confs, acc, color="blue", linewidth=.1, edgecolor="black", align="edge", width=1 / num_bins)
+    binned_confs = binned_confs.numpy()
+    binned_accs = acc.cpu().numpy()
+    # plt.bar(binned_confs.mean(0), binned_accs.mean(0), yerr=binned_accs.std(0), color="blue", linewidth=.1, edgecolor="black", align="edge", width=1 / num_bins)
+    plt.bar(fix_conf, binned_accs.mean(0), color="blue", linewidth=.1, edgecolor="black", align="edge", width= 0.2 )
+    # plt.bar(binned_confs.data.numpy(), acc.data.cpu().numpy(), color="blue", edgecolor="black", align="edge", width=1 / num_bins)
     plt.plot(np.linspace(0, 1, 1000), np.linspace(0, 1, 1000), "--")
-    plt.annotate(f"ECE: {ece * 100:.2f}%",
+    plt.annotate(f"ECE: {ece.cpu().numpy().mean() * 100:.2f}%",
                  xy=(140, 240),
                  xycoords='axes points',
                  size=20,
@@ -104,27 +117,105 @@ def _plot_reliability(save_dir, m, reliability_diag, ece, freq, dset_label_info)
                  bbox=dict(boxstyle='round', fc="#f6b2b2", color="#f6b2b2"))
     plt.xlim([0.0, 1.0])
     plt.ylim([0.0, 1.0])
-    plt.xticks([.0, .5, 1.])
-    plt.yticks([.0, .5, 1.])
+    plt.xticks([.0,0.2,0.4,0.6,0.8,1.])
+    plt.yticks([.0,0.2,0.4,0.6,0.8,1.])
     plt.xlabel('Confidence')
     plt.ylabel('Accuracy')
     plt.title('')
     plt.gcf().subplots_adjust(right=.95, left=.14, bottom=.15)
-    plt.savefig(f"{save_dir}/ece_{m}.png", dpi=200)
-    plt.savefig(f"{save_dir}/ece_{m}.pdf")
+    plt.savefig(f"{save_dir}/ece/ece_calibration_{m}.png", dpi=200)
+    plt.savefig(f"{save_dir}/ece/ece_calibration_{m}.pdf")
     plt.close(fig)
 
 
 def plot_reliability(save_dir, m, reliability_diag=None, ece=None, freq=None, dset_label_info=None, from_cache=True):
     if from_cache:
-        with open(f"{save_dir}/reliability.npy", "rb") as f:
+        # with open(f"{save_dir}/reliability.npy", "rb") as f:
+        with open(f"{save_dir}/ece/cache_ece_{m}.pickle", "rb") as f:
             d = np.load(f, allow_pickle=True).item()
     else:
-        d = {"reliability_diag": reliability_diag, "ece": ece, "freq": freq, "dset_label_info": dset_label_info}
+        print('No reliability exists!')
+        # d = {"reliability_diag": reliability_diag, "ece": ece} # , "freq": freq, "dset_label_info": dset_label_info}
+        # with open(f"{save_dir}/cache_ece_{m}.pickle", "wb") as f:
+        #     np.save(f, d)
 
     _plot_reliability(save_dir, m, **d)
 
+##### averaging across corruptions #####
+PLOT_SEVERITIES = [0, 1, 2, 3, 4, 5]
+SEVERITIES = [1, 2, 3, 4, 5]
+PLOT_CORRUPTIONS = [0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+from src.get_data import NOISE_TYPES
 
-def plot_cifar10c(save_dir, m, model_to_noise_to_severity_dict):
-    # TODO
+def severity_vs_error(info_corrupt):
+    # for one model: avg cross all noise types for each severity level
+    # x: 0 - 5, y: error (%)
+    mean_corrupt_error = []
+    std_corrupt_error = []
+    for severity in SEVERITIES:
+        corrupt_error = []
+        for noise in NOISE_TYPES:
+            _corrupt_error = info_corrupt[noise][severity]
+            corrupt_error.append(_corrupt_error)
+        mean_corrupt_error.append(np.mean(corrupt_error))
+        std_corrupt_error.append(np.std(corrupt_error))
+    assert len(mean_corrupt_error) == len(SEVERITIES)
+    assert len(std_corrupt_error) == len(SEVERITIES)
+    return mean_corrupt_error, std_corrupt_error
+
+def plot_cifar10c(save_dir, models, legend_loc="lower left", name='sev_acc', _xticks=["1", "2", "3", "4", "5"]):
+
+    puzzmix = "red"
+    augmix = "blue"
+    manimix_base = "green"
+    nfm_a02_1_04_02 = "violet"
+    nfm_1e8_1_04_02 = "navy"
+    nfm_a1_0_0_0 = "coral"
+    nfm_a1_1_0_0 = "orange"
+    nfm_a1_1_04_02 = "grey"
+
+    legend = {0: 'manifold', 1: 'nfm_a02_1_04_02', 2: 'nfm_a1_0_0_0', 3: 'nfm_a1_1_0_0',
+              4: 'nfm_a1_1_04_02', 5: 'nfm_1e8_1_04_02', 6: 'augmix', 7: 'puzzmix'}
+
+    print(models)
+    # ['arch_preactresnet18_alpha_0.0_manimixup_0_addn_0.0_multn_0.0_seed_1.pt', 
+    # 'arch_preactresnet18_alpha_0.2_manimixup_1_addn_0.4_multn_0.2_seed_4.pt', 
+    # 'arch_preactresnet18_alpha_1.0_manimixup_0_addn_0.0_multn_0.0_seed_1.pt', 
+    # 'arch_preactresnet18_alpha_1.0_manimixup_1_addn_0.0_multn_0.0_seed_1.pt', 
+    # 'arch_preactresnet18_alpha_1.0_manimixup_1_addn_0.4_multn_0.2_seed_1.pt', 
+    # 'arch_preactresnet18_alpha_1e-08_manimixup_0_addn_0.4_multn_0.0_seed_1.pt', 
+    # 'augmix_seed_1.pt', 'puzzelmix_cifar10_seed_1.pt']
+    N = len(models)
+    bar_width = 0.1
+
+    fig, ax = plt.subplots(1, 1, figsize=(9, 6))
+    # ax.set_prop_cycle(color=["2e1e3b", "#413d7b", "#37659e", "#348fa7", "#40b7ad", "#8bdab2"])
+    ax.set_prop_cycle(color=[manimix_base, nfm_a02_1_04_02, nfm_a1_0_0_0, nfm_a1_1_0_0, nfm_a1_1_04_02, nfm_1e8_1_04_02, augmix, puzzmix])
+
+    x = np.arange(len(SEVERITIES))
+    
+    with open(f'{save_dir}/cifar10c.npy', 'rb') as f:
+        info_corrupt = np.load(f, allow_pickle=True).item()
+    
+    for i, model in enumerate(range(N)):
+        _model = models[model]
+        _model_info = info_corrupt[_model]
+        y, yerr = severity_vs_error(_model_info)
+        # ax.errorbar(x, y, yerr=yerr, label=_model, elinewidth=2)
+        ax.bar(np.arange(len(SEVERITIES)) + i * bar_width, np.array(y) * 100, width=bar_width, edgecolor='white', label=legend[i])
+        # ax.errorbar(x, y, yerr=yerr, label=_model, fmt='o', elinewidth=2)
+
+    plt.legend(loc=legend_loc, prop={'size': 13})
+    ax.set_ylabel("Accuracy (%)")
+    ax.set_xlabel("Severity Level")
+    plt.xticks([r + 3 * bar_width for r in range(len(SEVERITIES))], _xticks)
+    plt.ylim([0, 100])
+
+    plt.tight_layout()
+    plt.savefig("%s/cifar10c/%s.pdf" % (save_dir, name))
+    plt.savefig("%s/cifar10c/%s.png" % (save_dir, name))
+    # plt.savefig("%s/%s.svg" % (plot_dir, name), format='svg')
+    plt.clf()
+    plt.close(fig)
+    
     return
